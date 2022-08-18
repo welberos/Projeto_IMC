354366c03aaf99cf84cdeeac6673ffdcb9607176
Welber Santos <111401566+welberos@users.noreply.github.com>
2022-08-18 12:07:02 -0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,9 +1,9 @@
-function notANumber(value){
+export function notANumber(value){
   return isNaN(value)  || value == ""
 }
-module.exports = notANumber
 
-function calculateIMC(weight, height) {
+
+export function calculateIMC(weight, height) {
   return (weight / ((height /100) ** 2)).toFixed(2)
 } 
-module.exports = calculateIMC
+
