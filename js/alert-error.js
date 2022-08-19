49b45ce15d2a727950131629a2849ae0b4d2a8c6
Welber Santos <111401566+welberos@users.noreply.github.com>
2022-08-18 21:20:22 -0300

    Update alert-error.js

--- js/alert-error.js
+++ js/alert-error.js
@@ -1,9 +1,9 @@
 export const AlertError = {
   element: document.querySelector('.alert-error'),
   open() {
-   AlertError.element.classList.add('open')
+    AlertError.element.classList.add('open')
   },
-  close(){
+  close() {
     AlertError.element.classList.remove('open')
   }
 }
